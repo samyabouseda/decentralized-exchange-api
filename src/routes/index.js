dfa4ade79c5465f2dfc6adf020b5a994d06db842
samyabouseda <samy@abouseda.ch>
2019-11-28 17:57:47 +0100

    Initial commit

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import { OK } from 'http-status-codes'
+import userRoutes from './user-routes'
+const router = Router()
+
+router.use('/users', userRoutes)
+
+router.get('/', (req, res) => {
+	res.status(OK).json({ message: 'Connected' })
+})
+
+export default router
